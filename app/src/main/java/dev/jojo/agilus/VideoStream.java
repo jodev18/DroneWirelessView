cbda49f41de0483faa10902d984bef9015e5eb45
Jojo <josiahbuhayan@gmail.com>
2017-12-22 16:55:27 +0800

    added stream activity.

---
+++ app/src/main/java/dev/jojo/agilus/VideoStream.java
@@ -0,0 +1,13 @@
+package dev.jojo.agilus;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class VideoStream extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_video_stream);
+    }
+}
